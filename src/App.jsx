c3f393d824dd480211ca2f48a4d4b93aaff89f0b
Vaibhav Nagpal <vaibhavnagpal.dev@gmail.com>
2025-09-07 18:04:09 +0530

    feat: initialize React project with Vite, Tailwind CSS, and ESLint

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="flex items-center justify-center w-full h-full bg-black flex-col">
+      <h1>Hello Vite + React!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  )
+}
+export default App;
